ef3b941af202e37bf38007079076e5aa4c5c63b3
Diana Amirzadyan <amirzadiana@gmail.com>
2025-09-07 18:40:31 +0400

    Initial Commit

---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by Diana Amirzadyan on 07.09.25.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
